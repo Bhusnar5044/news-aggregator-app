3189ddaeadc062000a2443b83f4fc7055c5a38af
Bhusnar5044 <bhusnarpradip@gmail.com>
2020-04-12 19:54:16 +0530

    add some extra effects

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,14 @@
 //get search element
 let inputele = document.getElementById("search");
 
-iterateNews();//news iterator function
+window.onload = ()=>{
+
+    document.querySelector('.loader').classList.add('fadeOut');
+    document.querySelector('.loader').classList.remove('loader');
+    
+    iterateNews();
+    //news iterator function
+};
 
 //start news top-articles display code
 function iterateNews(){
@@ -10,6 +17,7 @@ function iterateNews(){
 
         var url = 'http://newsapi.org/v2/top-headlines?' +
                 'country=in&' +
+                'pageSize=80&' +
                 'apiKey=f95bc5db28994a1da7895f7e38cdb7f3'; 
         display(url);
     }
@@ -21,6 +29,7 @@ function myFunction(){
 
     let url = 'http://newsapi.org/v2/everything?' +
           'q='+inputele.value+'&' +
+          'pageSize=80&' +
           'from=2020-04-10&' +
           'sortBy=popularity&' +
           'apiKey=f95bc5db28994a1da7895f7e38cdb7f3';
@@ -45,6 +54,7 @@ document.getElementById("getfilter").addEventListener("click", function(event){
     let url = 'http://newsapi.org/v2/top-headlines?' +
     'country='+countryTxt+'&' +
     'category='+categoryTxt+'&' +
+    'pageSize=80&' +
     'from='+datefrom+'&' +
     'to='+dateTo+'&' +
     'sortBy=popularity&' +
@@ -81,9 +91,11 @@ function display(url){
     
                 });
                 
-                document.querySelector('#news-articles').innerHTML=output;
+                document.querySelector('#news-articles').innerHTML = output;
                 document.querySelector('#news-articles').style.display = '';
+                showArticles();//pagiantion function
                 document.querySelector('.not-found').style.display = 'none';
+                
     
             }else if(data.totalResults === 0){
                 document.querySelector('.not-found').style.display = 'block';
@@ -173,3 +185,34 @@ fetch('src/jsonData/category.json')
     });
 //end set filter section option values
 
+
+const newsArticles=document.getElementById('news-articles').children;
+console.log(newsArticles);
+const maxArticles= 16;
+let curindex = 1;
+document.getElementById('prev').addEventListener('click', ()=>{
+    curindex--;
+    showArticles();
+});
+
+document.getElementById('next').addEventListener('click', ()=>{
+    curindex++;
+    showArticles();
+});
+
+function showArticles(){
+    // let index = parseInt(resultCount/maxArticles);
+
+    for(let i=0; i< newsArticles.length; i++){
+        console.log(newsArticles[i]);
+        newsArticles[i].classList.add("hide");
+
+        if(i >= (curindex*maxArticles)-maxArticles && i < curindex * maxArticles){
+            newsArticles[i].classList.add('show');
+            newsArticles[i].classList.remove("hide");
+        }
+    }
+
+    document.getElementById('page').innerHTML = curindex;
+  
+}
